8287a967da33f77f6106dc354e30d8fbfb401712
yuuki <1308529227@qq.com>
2022-04-27 11:15:12 +0800

    初始化项目

---
+++ src/test/java/com/yuuki/YuukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuukiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
