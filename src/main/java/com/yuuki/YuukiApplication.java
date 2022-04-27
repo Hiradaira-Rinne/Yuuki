5fccab19664db2150a259aa0b100ad96bd6c7cc2
yuuki <1308529227@qq.com>
2022-04-27 10:55:34 +0800

    初始化项目

---
+++ src/main/java/com/yuuki/YuukiApplication.java
@@ -0,0 +1,13 @@
+package com.yuuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuukiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuukiApplication.class, args);
+    }
+
+}
